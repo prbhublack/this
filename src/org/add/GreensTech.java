b5cbac380f6c497cb8d67ed61d33046afd91be01
prabhugreens <prabhhuganesh@gmail.com>
2019-10-19 11:36:24 +0530

    today

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,34 @@
+package org.add;
+
+public class GreensTech 
+{
+public void greensOmr() 
+{
+	System.out.println("Dhuraipakkam");
+}
+public void greensAdayar() 
+{
+	System.out.println("Adayar branch");
+}
+public void greensTambaram()
+{
+System.out.println("Tambaram branch");	
+}
+public void greensVelachery()
+{
+	System.out.println("Velachery branch");
+}
+public void greensAnnanagar() {
+	System.out.println("Annanagar branch");
+
+}
+public static void main(String[] args) {
+	GreensTech a=new GreensTech();
+	a.greensAdayar();
+	a.greensAnnanagar();
+	a.greensOmr();
+	a.greensTambaram();
+	a.greensVelachery();
+}
+
+}
